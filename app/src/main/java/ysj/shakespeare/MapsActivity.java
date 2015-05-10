b75c970386bca7d9bd6c8423802db0f0a1ab87c9
michael.carr <michael.carr@cs2s.cs>
2015-05-10 18:11:15 +0100

    Added the new java files and the apk and keystore

---
+++ app/src/main/java/ysj/shakespeare/MapsActivity.java
@@ -0,0 +1,120 @@
+package ysj.shakespeare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+
+        Button listButton = (Button) findViewById(R.id.listButton);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MapsActivity.this,ListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53.959965, -1.087298), 14.0f));
+        Sites sites = Sites.get(this);
+
+        for(Site current : sites.getAllSites()){
+            Log.e("Get LatLng", ""+ current.getLatLng());
+            Log.e("Get Name",""+current.getName());
+            Marker m = mMap.addMarker(new MarkerOptions().position(current.getLatLng()).title(current.getName()));
+            sites.setMarkerForSite(m,current);
+        }
+
+        //Set the site to show when the InfoWindow is clicked
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(MapsActivity.this, SiteInformation.class);
+                Site selected = Sites.get(MapsActivity.this).getSite(marker);
+                int index = Sites.get(MapsActivity.this).getAllSites().indexOf(selected);
+                intent.putExtra("index", index);
+                startActivity(intent);
+            }
+        });
+
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            //Set the info window to the name of the site selected and the icon
+            public View getInfoContents(Marker marker) {
+                View v = getLayoutInflater().inflate(R.layout.infowindow_layout, null);
+                Site selected = (Site) Sites.get(MapsActivity.this).getSite(marker);
+                TextView markerLabel = (TextView) v.findViewById(R.id.marker_label);
+                markerLabel.setText(selected.getName());
+                return v;
+            }
+        });
+
+    }
+
+}
